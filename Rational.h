f1a75646d2f4b8d4228b90b8f1efea537d598be0
Haykel Sriha <haykel.sriha@ecole.ensicaen.fr>
2023-09-18 21:47:39 +0200

    "just added a comment"

---
+++ Rational.h
@@ -0,0 +1,10 @@
+class Rational {
+private : 
+    long int num , denom ;
+public :
+    Rational ();
+    Rational (long int num, long int denom);
+    Rational (const Rational & rational);
+    /*zjkbjhdbc*/
+
+}
